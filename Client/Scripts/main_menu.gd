ebedc30adcfda1cd5a908beabfc14c456c2afde5
tsier <t.sierzputowski0@gmail.com>
2024-11-03 14:22:33 +0100

    lower level connection

--- Client/Scripts/main_menu.gd
+++ Client/Scripts/main_menu.gd
@@ -3,12 +3,23 @@ extends Node
 @onready var server_ip = $ConnectButton/ServerIpInput
 @onready var port = $ConnectButton/PortInput
 
+var tcp : StreamPeerTCP = StreamPeerTCP.new()
+var udp : PacketPeerUDP = PacketPeerUDP.new()
+
+var status : StreamPeerTCP.Status = StreamPeerTCP.STATUS_NONE
+
 func _on_connect_button_pressed() -> void:
-	print("trying to connect...")
-	
-	var client = ENetMultiplayerPeer.new()
-	client.create_client(server_ip.text, int(port.text))
-	multiplayer.multiplayer_peer = client
-	
-	await multiplayer.connected_to_server
-	Network._send_message("Hello server, it's me client")
+	if status == StreamPeerTCP.STATUS_NONE:
+		print("trying to connect...")
+		tcp.connect_to_host(server_ip.text, port.text.to_int())
+		udp.connect_to_host(server_ip.text, port.text.to_int())
+		status = StreamPeerTCP.STATUS_CONNECTING
+	elif status == StreamPeerTCP.STATUS_CONNECTED:
+		tcp.put_string("Hello server, it's me client")
+		udp.put_packet("Hello server, it's me client".to_ascii_buffer())
+	elif status == StreamPeerTCP.STATUS_CONNECTING:
+		print("trying to finalize connection")
+		tcp.poll()
+		status = tcp.get_status()
+	else:
+		print("ERROR")
--- Server/server.gd
+++ Server/server.gd
@@ -1,34 +1,44 @@
 extends Node
 
-var client1_id : int = -1
-var client2_id : int = -1
+var udp : UDPServer = UDPServer.new()
+var udps : Array[PacketPeerUDP]
+
+var tcp : TCPServer = TCPServer.new()
+var tcps : Array[StreamPeerTCP]
+#var cfg : TLSOptions
 
 func _ready() -> void:
-	multiplayer.peer_connected.connect(self._on_client_connect)
-	multiplayer.peer_disconnected.connect(self._on_client_disconnect)
+	udp.listen(8080)
+	tcp.listen(8080, "172.18.219.106")
+	#var key : CryptoKey = load("res://Server/key.key")
+	#var cert : X509Certificate = load("res://Shared/cert.crt")
+	#cfg = TLSOptions.server(key, cert)
+	#dtls.setup(cfg)
+	print("server is listening on port 8080")
+
+func _process(_delta : float) -> void:
+	while tcp.is_connection_available():
+		var peer : StreamPeerTCP = tcp.take_connection()
+		tcps.append(peer)
+		print("tcp peer connected")
 	
-	var server = ENetMultiplayerPeer.new()
-	if server.create_server(8080, 2) == OK:
-		print("Server is running on port 8080")
-	else:
-		printerr("Something went wrong, unable to cerate a server")
-		get_tree().quit()
-	multiplayer.multiplayer_peer = server
-
-func _on_client_connect(clientID : int) -> void:
-	if client1_id == -1:
-		client1_id = clientID
-		Network._send_message_id(client1_id, "You connected as the first player, id: " + str(client1_id))
-	else:
-		client2_id = clientID
-		Network._send_message_id(client2_id, "You connected as the second player, id: " + str(client2_id))
-		Network._send_message_id(client2_id, "First player id: " + str(client1_id))
-		Network._send_message_id(client1_id, "Second player connected, id: " + str(client2_id))
-	print("Client connected, id: " + str(clientID))
-
-func _on_client_disconnect(clientID : int) -> void:
-	if client1_id == clientID:
-		client1_id = -1
-	else:
-		client2_id = -1
-	print("Client disconnected, id: " + str(clientID))
+	for peer in tcps:
+		peer.poll()
+		var status : StreamPeerTCP.Status = peer.get_status()
+		if status == StreamPeerTCP.STATUS_CONNECTING:
+			continue
+		if status == StreamPeerTCP.STATUS_NONE or status == StreamPeerTCP.STATUS_ERROR:
+			tcps.erase(peer)
+			continue
+		while peer.get_available_bytes() > 0:
+			print("tcp: ", peer.get_string(), ", ", peer.get_connected_host())
+	
+	udp.poll()
+	while udp.is_connection_available():
+		var peer: PacketPeerUDP = udp.take_connection()
+		udps.append(peer)
+		print("udp peer connected")
+	
+	for peer in udps:
+		while peer.get_available_packet_count() > 0:
+			print("udp: ", peer.get_packet().get_string_from_ascii(), ", ", peer.get_packet_ip())
