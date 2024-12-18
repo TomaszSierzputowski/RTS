bbce02a8fb52cfdcd8b62589216037a2cb15b0ac
tsier <t.sierzputowski0@gmail.com>
2024-12-18 12:08:42 +0100

    Revert "units"
    This reverts commit 24e27b2207b1ee564a8c5e952aff0e57a3476996.

--- Server/Game/Building.gd
+++ Server/Game/Building.gd
@@ -2,39 +2,19 @@ extends Object
 
 class_name Building
 
-# Podstawowe właściwości
 var id: int
-var owner_id: int
 var position: Vector2
+var owner_id: int
 var size: Vector2
-var health: int = 1000  # Początkowa wytrzymałość budynku
-var max_health: int = 1000  # Maksymalna wytrzymałość budynku
 
-# Funkcja inicjalizująca
-func _init(building_id: int, start_position: Vector2, owner: int, building_size: Vector2) -> void:
-	id = building_id
-	position = start_position
-	owner_id = owner
-	size = building_size
+func _init(id: int, position: Vector2, owner_id: int, size: Vector2) -> void:
+	self.id = id
+	self.position = position
+	self.owner_id = owner_id
+	self.size = size
 
-# Otrzymywanie obrażeń
-func take_damage(amount: int) -> void:
-	health -= amount
-	print("Budynek ID:", id, "otrzymał obrażenia:", amount, "pozostałe zdrowie:", health)
-	if health <= 0:
-		health = 0
-		die()
-
-# Logika śmierci budynku
-func die() -> void:
-	print("Budynek ID:", id, "został zniszczony")
-	emit_signal("destroyed", id, owner_id)
-	
 func is_at_position(pos: Vector2) -> bool:
 	return position.x <= pos.x and pos.x < position.x + size.x and position.y <= pos.y and pos.y < position.y + size.y
 
-
-
-
-
-	
+func destroy() -> void:
+	print("Building ID: " + str(id) + " has been destroyed.")
--- Server/Game/Game.gd
+++ Server/Game/Game.gd
@@ -1,40 +1,26 @@
 extends Node
 
-class_name Game
+var map_size = Vector2()
+var walkable_map = []
+var characters = [] 
+var buildings = []  
+var player_resources = {}
+var character_id_counter = 0
+var building_id_counter = 0
 
-var map_size: Vector2 = Vector2()
-var walkable_map: Array = []
-var players: Array = []
-var character_id_counter: int = 0
-var building_id_counter: int = 0
-
-signal game_initialized()
-signal character_created(character_id: int, owner_id: int, position: Vector2)
-signal building_created(building_id: int, owner_id: int, position: Vector2)
-signal resources_changed(player_id: int, resource_type: String, new_amount: int)
+signal character_created(character_id, owner_id, position)
+signal building_created(building_id, owner_id, position)
+signal resources_changed(player_id, resource_type, new_amount)
 
 func _ready() -> void:
 	pass
-	
+
 func _process(delta: float) -> void:
 	pass
 
-func start_new_game(map_path: String, player1_id: int, player2_id: int) -> void:
-	# Wczytaj mapę
-	load_map_from_file(map_path)
-	
-	# Utwórz dwóch graczy
-	var player1 = Player.new(player1_id)
-	var player2 = Player.new(player1_id)
-	players.append(player1)
-	players.append(player2)
-	
-	emit_signal("game_initialized")
-	print("Nowa gra rozpoczęta! Dwóch graczy zostało stworzonych.")
-
 func load_map_from_file(file_path: String) -> void:
 	var file = FileAccess.open(file_path, FileAccess.READ)
-	if file:
+	if file != null:
 		var json_data = file.get_as_text()
 		var json = JSON.new()
 		var result = json.parse(json_data)
@@ -43,73 +29,100 @@ func load_map_from_file(file_path: String) -> void:
 			var data = json.get_data()
 			map_size = Vector2(data["map_size"]["width"], data["map_size"]["height"])
 			walkable_map = data["walkable_map"]
-			print("Mapa załadowana z pliku:", file_path)
+			print("Mapa zaladowana z pliku:", file_path)
 		else:
-			print("Błąd podczas parsowania JSON:", result)
+			print("Blad podczas parsowania JSON:", result)
 		file.close()
 	else:
-		print("Nie udało się otworzyć pliku:", file_path)
+		print("Nie udalo sie otworzyc pliku:", file_path)
 
-func get_player(player_id: int) -> Player:
-	for player in players:
-		if player.id == player_id:
-			return player
-	print("Gracz o ID:", player_id, "nie został znaleziony")
-	return null
-
-func handle_character_creation(owner_id: int, position: Vector2) -> int:
-	var player = get_player(owner_id)
-	if player:
-		if can_spawn_character_at_position(position):
-			var character_id = character_id_counter
-			character_id_counter += 1
-			player.add_character(Character.new(character_id, position, owner_id))
-			emit_signal("character_created", character_id, owner_id, position)
-			print("Postać ID:", character_id, "utworzona dla gracza ID:", owner_id)
-			return character_id
-		else:
-			print("Nie można stworzyć postaci na pozycji:", position)
-	return -1
-
-func handle_building_creation(owner_id: int, position: Vector2, size: Vector2) -> int:
-	var player = get_player(owner_id)
-	if player:
-		if can_place_building_at_position(position, size):
-			var building_id = building_id_counter
-			building_id_counter += 1
-			player.add_building(Building.new(building_id, position,owner_id, size))
-			emit_signal("building_created", building_id, owner_id, position)
-			print("Budynek ID:", building_id, "utworzony dla gracza ID:", owner_id)
-			return building_id
-		else:
-			print("Nie można stworzyć budynku na pozycji:", position)
-	return -1
 
 func can_spawn_character_at_position(position: Vector2) -> bool:
-	if position.x < 0 or position.x >= map_size.x or position.y < 0 or position.y >= map_size.y:
+	var x = int(position.x)
+	var y = int(position.y)
+	if x < 0 or x >= map_size.x or y < 0 or y >= map_size.y:
 		return false
-	if walkable_map[int(position.y)][int(position.x)] != 0:
+	if walkable_map[y][x] != 0:
 		return false
-	for player in players:
-		for character in player.characters:
-			if character.is_at_position(position):
-				return false
+	for character in characters:
+		if character.is_at_position(position):
+			return false
+	for building in buildings.values():
+		if building["position"] == position:
+			return false
 	return true
 
 func can_place_building_at_position(position: Vector2, size: Vector2) -> bool:
-	var x_end = int(position.x + size.x)
-	var y_end = int(position.y + size.y)
+	var x_start = int(position.x)
+	var y_start = int(position.y)
+	var x_end = x_start + int(size.x)
+	var y_end = y_start + int(size.y)
 
 	if x_end > map_size.x or y_end > map_size.y:
 		return false
 
-	for y in range(int(position.y), y_end):
-		for x in range(int(position.x), x_end):
+	for y in range(y_start, y_end):
+		for x in range(x_start, x_end):
 			if walkable_map[y][x] != 0:
 				return false
-			for player in players:
-				for building in player.buildings:
-					if building.is_at_position(Vector2(x, y)):
-						return false
+
+	for building in buildings:
+		for y in range(y_start, y_end):
+			for x in range(x_start, x_end):
+				if building.is_at_position(Vector2(x, y)):
+					return false
 
 	return true
+
+func create_character(start_position: Vector2, owner_id: int) -> int:
+	if can_spawn_character_at_position(start_position):
+		var character_id = character_id_counter
+		character_id_counter += 1
+		var character = Character.new(character_id, start_position, owner_id)
+		characters.append(character)
+		emit_signal("character_created", character_id, owner_id, start_position)
+		print("Gracz o ID:", owner_id, " stworzyl nowa postac o ID:", character_id, " na pozycji:", start_position)
+		return character_id
+	else:
+		print("Gracz o ID:", owner_id, " nie moze utworzyc postac na pozycji:", start_position)
+		return -1
+
+func create_building(start_position: Vector2, owner_id: int, size: Vector2) -> int:
+	if can_place_building_at_position(start_position, size):
+		var building_id = building_id_counter
+		building_id_counter += 1
+		var building = Building.new(building_id, start_position, owner_id, size)
+		buildings.append(building)
+		emit_signal("building_created", building_id, owner_id, start_position)
+		print("Gracz o ID:", owner_id, " stworzyl nowy budynek o ID:", building_id, " na pozycji:", start_position)
+		return building_id
+	else:
+		print("Gracz o ID:", owner_id, " nie moze utworzyc budynku na pozycji:", start_position)
+		return -1
+
+func add_resources(player_id: int, resource_type: String, amount: int) -> void:
+	if player_id in player_resources:
+		player_resources[player_id][resource_type] += amount
+		var new_amount = player_resources[player_id][resource_type]
+		emit_signal("resources_changed", player_id, resource_type, new_amount)
+		print("Gracz o ID:", player_id, " zwiekszyl zasob:", resource_type, " o:", amount, "jego obecny stan:", player_resources[player_id][resource_type])
+	else:
+		print("Gracz o ID:", player_id, " nie istneje")
+
+func deduct_resources(player_id: int, resource_type: String, amount: int) -> bool:
+	if player_id in player_resources and player_resources[player_id][resource_type] >= amount:
+		player_resources[player_id][resource_type] -= amount
+		var new_amount = player_resources[player_id][resource_type]
+		emit_signal("resources_changed", player_id, resource_type, new_amount)
+		print("Gracz o ID:", player_id, " pomniejszony zasob:", resource_type, " o:", amount, " lacznie ma", player_resources[player_id][resource_type])
+		return true
+	else:
+		print("Gracz o ID:", player_id, " nie istnieje lub nie ma wystarczajacej liczby zasobow")
+		return false
+
+func get_resources(player_id: int) -> Dictionary:
+	if player_id in player_resources:
+		return player_resources[player_id]
+	else:
+		print("Gracz o ID:", player_id, "nie zostal znaleziony")
+		return {}
