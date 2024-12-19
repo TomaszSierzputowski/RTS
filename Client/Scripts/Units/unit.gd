131138ffe130b5cc3f772cebc094210e32bef2c2
Kinga <272354@student.pwr.edu.pl>
2024-12-19 22:15:10 +0100

    added ability to change opacity related to hp level

--- Client/Scripts/Units/unit.gd
+++ Client/Scripts/Units/unit.gd
@@ -1,28 +1,36 @@
 extends CharacterBody2D
 
 @export var selected: bool = false
-@onready var box = $box
+@onready var highlight = $highlight
 @onready var unit_1_red = $unit_1_red
 @onready var unit_1_blue = $unit_1_blue
-@onready var unit_2_red = $unit_2_red
-@onready var unit_2_blue = $unit_2_blue
-
-var visible_array = [true, false, false, false]
+@onready var unit_type
+@export var hp: int
+var max_hp: int = 100
+@export var unit_color: bool = false
 
 signal was_selected
 signal was_deselected
 
 func _ready() -> void:
-	pass
+	if unit_color == false:
+		unit_type = $unit_1_red
+	else:
+		unit_type = $unit_1_blue
+	unit_type.visible = true
 
  
 func _process(delta: float) -> void:
-	box.visible = selected
+	highlight.visible = selected
+	var alpha = hp / float(max_hp)
+	unit_type.self_modulate.a = clamp(alpha, 0.0, 1.0)
+
+	
 	
 func set_selected(value: bool) -> void:
 	if selected != value:
 		selected = value
-		box.visible = value
+		highlight.visible = value
 		if selected:
 			emit_signal("was_selected", self)
 		else:
@@ -35,3 +43,5 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 			set_selected(true)
 		elif event.button_index == MOUSE_BUTTON_LEFT and event.pressed and selected == true:
 			set_selected(false)
+		elif event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			hp -= 5
